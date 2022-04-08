6b913e8b33250ca77d256cea853e31c290e916fb
Oleg Freidkin <oleg.freidkin@gmail.com>
2022-04-08 18:15:11 +0300

    Тесты не работают

---
+++ ClassLibrary2/UserServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace SocialNetwork.BLL.Tests
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public void RegistrationThrowExceptionsTest()
+        {
+            var userService = new UserService();
+
+            var testuser = new UserRegistrationData();
+            Assert.Throws<ArgumentNullException>(() => userService.Register(testuser));
+
+            testuser.FirstName = "Ivan";
+        }
+
+        [Fact]
+        public void FindByEmailThrowExceptionsTest()
+        {
+            var userService = new UserService();
+            Assert.Throws<UserNotFoundException>(() => userService.FindByEmail("thereisnosuchemail@no.no"));
+        }
+
+
+    }
+}
